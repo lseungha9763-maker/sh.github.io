ad839dd5edaf4bdf4055a34341608363be41b173
lseungha9763-maker <lseungha9763@gmail.com>
2025-09-15 15:18:16 +0900

    first upload
    first upload& first commit

---
+++ project/resources/js/style.js
@@ -0,0 +1,174 @@
+/* campland-N1 */
+(function() {
+  $(function() {
+    $(".campland-N1[id=\'kTMD6zDm6l\']").each(function() {
+      const $block = $(this);
+      let isMobileMenuInitialized = false;
+      let isDesktopMenuInitialized = false;
+      // 모바일 메뉴 초기화
+      function initMobileMenu() {
+        if (isMobileMenuInitialized) return;
+        const $btnMomenu = $block.find(".btn-momenu");
+        $btnMomenu.off("click").on("click", function() {
+          if ($block.hasClass("block-active")) {
+            $block.removeClass("block-active");
+          } else {
+            $block.addClass("block-active");
+          }
+          $block.find(".header-gnbitem").removeClass("item-active");
+          $block.find(".header-sublist").removeAttr("style");
+        });
+        // header-gnbitem 클릭 이벤트
+        $block.find(".header-gnbitem").each(function() {
+          const $this = $(this);
+          const $thisLink = $this.find(".header-gnblink");
+          const $sublist = $this.find(".header-sublist");
+          if ($sublist.length) {
+            $thisLink.off("click").on("click", function(event) {
+              event.preventDefault();
+              const $clickedItem = $(this).parents(".header-gnbitem");
+              if (!$clickedItem.hasClass("item-active")) {
+                $block.find(".header-gnbitem").removeClass("item-active");
+                $block.find(".header-sublist").stop().slideUp(300);
+              }
+              $clickedItem.toggleClass("item-active");
+              $sublist.stop().slideToggle(300);
+            });
+          }
+        });
+        isMobileMenuInitialized = true;
+      }
+      // 데스크탑 메뉴 초기화
+      function initDesktopMenu() {
+        if (isDesktopMenuInitialized) return;
+        $block.find(".header-gnbitem").each(function() {
+          const $this = $(this);
+          const $thisLink = $this.find(".header-gnblink");
+          $thisLink.off("click");
+        });
+        isDesktopMenuInitialized = true;
+      }
+      // 해상도에 따른 메뉴 처리
+      function handleResize() {
+        if (window.innerWidth <= 992) {
+          if (!isMobileMenuInitialized) {
+            initMobileMenu();
+          }
+          isDesktopMenuInitialized = false;
+        } else {
+          if (!isDesktopMenuInitialized) {
+            initDesktopMenu();
+          }
+          isMobileMenuInitialized = false;
+        }
+      }
+      // 스크롤 시 메뉴 처리
+      function handleScroll() {
+        const $headerTop = $block.find(".header-top");
+        if ($headerTop.length) {
+          $block.addClass("top-menu-active");
+        }
+        if ($(window).scrollTop() === 0) {
+          $block.addClass("header-top-active");
+        }
+        $(window).scroll(function() {
+          if ($(window).scrollTop() > 0) {
+            $block.removeClass("header-top-active");
+          } else {
+            $block.addClass("header-top-active");
+          }
+        });
+      }
+      handleScroll();
+      // 전체 메뉴 열기/닫기 처리
+      function handleFullMenu() {
+        $block.find(".btn-allmenu").on("click", function() {
+          $block.find(".header-fullmenu").addClass("fullmenu-active");
+        });
+        $block.find(".fullmenu-close").on("click", function() {
+          $block.find(".header-fullmenu").removeClass("fullmenu-active");
+        });
+        $block.find(".fullmenu-gnbitem").each(function() {
+          const $this = $(this);
+          $this.on("mouseover", function() {
+            if (window.innerWidth > 992) {
+              $this.find(".fullmenu-gnblink").addClass("on");
+            }
+          });
+          $this.on("mouseout", function() {
+            if (window.innerWidth > 992) {
+              $this.find(".fullmenu-gnblink").removeClass("on");
+            }
+          });
+        });
+      }
+      handleFullMenu();
+      // 리사이즈 시마다 메뉴 동작 초기화
+      $(window).on("resize", function() {
+        handleResize();
+      });
+      handleResize();
+    });
+  });
+})();
+/* campland-N3 */
+(function() {
+  $(function() {
+    $(".campland-N3[id=\'rQMD6zDm9Y\']").each(function() {
+      const $block = $(this);
+      // Swiper
+      const swiper = new Swiper(".campland-N3[id=\'rQMD6zDm9Y\'] .swiper", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        autoplay: {
+          delay: 5000,
+        },
+        pagination: {
+          el: ".campland-N3[id=\'rQMD6zDm9Y\'] .swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".campland-N3[id=\'rQMD6zDm9Y\'] .swiper-button-next",
+          prevEl: ".campland-N3[id=\'rQMD6zDm9Y\'] .swiper-button-prev",
+        },
+      });
+      // Swiper Play, Pause Button
+      const pauseButton = $block.find(".swiper-button-pause");
+      const playButton = $block.find(".swiper-button-play");
+      playButton.hide();
+      pauseButton.show();
+      pauseButton.on("click", function() {
+        swiper.autoplay.stop();
+        playButton.show();
+        pauseButton.hide();
+      });
+      playButton.on("click", function() {
+        swiper.autoplay.start();
+        playButton.hide();
+        pauseButton.show();
+      });
+    });
+  });
+})();
+/* campland-N6 */
+(function() {
+  $(function() {
+    $(".campland-N6[id=\'pBmD6zDMkx\']").each(function() {
+      const $block = $(this);
+      // Swiper
+      const swiper = new Swiper(".campland-N6[id=\'pBmD6zDMkx\'] .swiper", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        autoplay: {
+          delay: 5000,
+        },
+        navigation: {
+          nextEl: ".campland-N6[id=\'pBmD6zDMkx\'] .swiper-button-next",
+          prevEl: ".campland-N6[id=\'pBmD6zDMkx\'] .swiper-button-prev",
+        },
+      });
+    });
+  });
+})();
